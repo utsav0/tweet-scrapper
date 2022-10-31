864abd3a34d49f07bbfd5d5e2302892db3329ca4
utsav <meenautsav1612@gmail.com>
2022-10-31 18:58:01 +0530

    changed the debugging to false

--- tweet_scrapper/settings.py
+++ tweet_scrapper/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["tweet-scrapper.up.railway.app"]
 
