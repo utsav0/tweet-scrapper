f4c46678082ba86d460c258120daec70801ef56e
utsav <meenautsav1612@gmail.com>
2022-11-01 08:34:19 +0530

    changed debugging to True

--- tweet_scrapper/settings.py
+++ tweet_scrapper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('django_secret_key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["tweet-scrapper.up.railway.app", '127.0.0.1']
 
